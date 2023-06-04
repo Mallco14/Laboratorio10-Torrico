9795d06c1f2ca7b9e09a5c0177393cefcbaa6ed6
Jhon Mallcco <jhonmallcco@gmail.com>
2023-06-03 23:15:57 -0500

    first commit

---
+++ MVCCodeFirts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCCodeFirts.MvcApplication" Language="C#" %>
---
+++ MVCCodeFirts/Migrations/202305301813322_v1.cs
@@ -0,0 +1,26 @@
+﻿namespace MVCCodeFirts.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.ProductId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
